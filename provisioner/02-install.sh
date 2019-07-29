1df5a6926b5648069727ef7504a5479bfd127e2a
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-07-29 11:31:56 +0200

    Remove obsolete provisioner

--- provisioner/02-install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd `dirname $0`
-
-echo " installing qemu "
-
-sudo apt-get install -y qemu-system sshpass
-
--- provisioner/05-box-up.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd `dirname $0`
-
-echo "starting qemu but detaching it from this shell, wait until a login prompt apears before running the next script"
-./box-up &
